08a91c933fdb1d4dfd03ad14f0a3d07136cd3c92
Brendan O'Dea <brendan.ode4@gmail.com>
2017-12-24 16:12:14 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
-# code your #position_taken? method here!
+def position_taken?(board, index)  # code your #position_taken? method here!
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+
+end
